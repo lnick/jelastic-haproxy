924c007f490bc260724291f4ace821ad0f197a06
QA <qa@localhost.localdomain>
2014-11-05 16:43:54 +0200

    fixed balancing script and config

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 function _setNeighbors(){
-return 0;
+    return 0;
 }
 
 function _rebuildCommon(){
-sudo /etc/init.d/cartridge restart;
+    sudo /etc/init.d/cartridge restart;
 }
 
 function _addCommonHost(){
-sed '/balance     roundrobin/a server server1 ${host}:80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    local last_node_id=$(cat ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf | grep -o "webserver[0-9]" | sed 's/webserver//g' | sort | tail -n1);
+    sed -i '/balance     roundrobin/a server webserver${$(( last_node_id + 1 ))} ${host}:80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 }
 
 function _removeCommonHost(){
-sed '/${host}:80/d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    sed -i '/${host}:80/d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 }
 
 
