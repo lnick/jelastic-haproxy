a0c2d4bfcecdf56af0d50def2b0d3089b1ec78fd
QA <qa@localhost.localdomain>
2014-11-05 15:01:35 +0200

    updated balancer script

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -5,13 +5,11 @@ return 0;
 }
 
 function _rebuildCommon(){
-rm -rf ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
-cp ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf.tpl ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 sudo /etc/init.d/cartridge restart;
 }
 
 function _addCommonHost(){
-sed '/#ip addresses of compute nodes are inserted after this string/a server webserver1 ${host}:80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+sed '/balance     roundrobin/a server server1 ${host}:80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 }
 
 function _removeCommonHost(){
