75877a0965b7db507f4dc91102d24ea4d972b321
SiryjVyiko <dz@hivext.net>
2014-11-07 16:45:48 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -9,13 +9,14 @@ function _rebuild_common(){
 }
 
 function _add_common_host(){
-    local last_node_id=$(cat ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf | grep -o "webserver[0-9]" | sed 's/webserver//g' | sort | tail -n1);
-    sed -i '/balance     roundrobin/a server webserver${$(( last_node_id + 1 ))} ${host}:80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    #local last_node_id=$(cat${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf | grep -o "webserver[0-9]" | sed 's/webserver//g' | sort | tail -n1);
+    sed -i '/balance     roundrobin/a server webserver1 '${host}':80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 }
 
 function _remove_common_host(){
-    sed -i '/${host}:80/d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    sed -i '/'${host}':80/d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
 }
+
 
 function _add_host_to_group(){
     return 0;
