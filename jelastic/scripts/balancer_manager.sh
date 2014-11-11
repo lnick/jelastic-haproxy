f4ef46f54d77e93ae9d640769512df2e44c0a72c
SiryjVyiko <dz@hivext.net>
2014-11-11 12:16:14 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -5,13 +5,23 @@ function _set_neighbors(){
 }
 
 function _rebuild_common(){
-    /etc/init.d/cartridge restart;
+    sed -i '/balance roundrobin/,$d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    cat ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf ${OPENSHIFT_HAPROXY_DIR}/hosts > ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf.tmp;
+    cp ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf.tmp ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    sed '/backend app/a balance roundrobin' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+    rm -rf ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf.tmp;
+    rm -rf ${OPENSHIFT_HAPROXY_DIR}/hosts;
+    ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/bin/haproxy -D -f ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+
 }
 
-function _add_common_host(){
-    #local last_node_id=$(cat${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf | grep -o "webserver[0-9]" | sed 's/webserver//g' | sort | tail -n1);
-    sed -i '/balance     roundrobin/a server webserver1 '${host}':80' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
+function _add_common_host(){    
+    touch ${OPENSHIFT_HAPROXY_DIR}/hosts;
+    count=$(cat /opt/repo/hosts | grep -o "webserver[0-9]" | sed 's/webserver//g' | sort | tail -n1);
+    let "count+=1";
+    echo "server webserver${count} ${host}:80" >> ${OPENSHIFT_HAPROXY_DIR}/hosts;
 }
+
 
 function _remove_common_host(){
     sed -i '/'${host}':80/d' ${OPENSHIFT_HAPROXY_DIR}/versions/1.5.3/conf/haproxy.conf;
