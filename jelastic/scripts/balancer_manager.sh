b6d3c7eaf6f875a1cad76fb83a2d3ac5606a0f82
SiryjVyiko <dz@hivext.net>
2014-11-13 17:39:59 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -8,7 +8,7 @@ function _rebuild_common(){
     sed -i '/roundrobin$/,$d' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
     cat ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf ${CARTRIDGE_HOME}/hosts > ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp;
     cp ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
-    sed '/backend app/a balance roundrobin' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
+    sed -i '/backend app/a balance roundrobin' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
     [ -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp ] && rm -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp;
     ${CARTRIDGE_HOME}/versions/1.5.3/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
 
