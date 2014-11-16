88305f90b09f72e9a19ddfe74ed438f90ac1b8fb
SiryjVyiko <dz@hivext.net>
2014-11-17 00:17:36 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -8,7 +8,7 @@ function _rebuild_common(){
     sed -i '/###LIST OF ENVIRONMENT APPSERVER NODES###$/,$d' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
     cat ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf ${CARTRIDGE_HOME}/hosts > ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp;
     cp ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
-    sed -i '/backend app/a balance roundrobin' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
+    sed -i '/backend app/a ###LIST OF ENVIRONMENT APPSERVER NODES###' ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf;
     [ -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp ] && rm -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf.tmp;
     su - jelastic -c "${CARTRIDGE_HOME}/versions/1.5.3/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/1.5.3/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
