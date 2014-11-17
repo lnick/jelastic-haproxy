31a46dd0948cc7936be596dcf9ce66838b8cf7a8
SiryjVyiko <dz@hivext.net>
2014-11-17 15:30:06 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -2,5 +2,5 @@
 
 function _setPassword() {
   sed -i '/stats auth/d' /opt/repo/versions/1.5.3/conf/haproxy.conf;
-  sed  '/stats enable/a stats auth $J_OPENSHIFT_APP_ADM_USER:$J_OPENSHIFT_APP_ADM_PASSWORD ' /opt/repo/versions/1.5.3/conf/haproxy.conf;
+  sed  '/stats enable/a stats auth ${J_OPENSHIFT_APP_ADM_USER}:${J_OPENSHIFT_APP_ADM_PASSWORD} ' /opt/repo/versions/1.5.3/conf/haproxy.conf;
 }
