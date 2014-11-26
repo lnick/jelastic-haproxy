f32b611c7e3ae41acfa141ae966283b274e21ee8
SiryjVyiko <dz@hivext.net>
2014-11-26 10:59:20 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 function _enableSSL(){
-        local err;
         doAction keystore DownloadKeys;
-        err=$?; [[ ${err} -gt 0 ]] && exit ${err};
 }
 
 function _disableSSL(){
-        local err;
         doAction keystore remove;
-        err=$?; [[ ${err} -gt 0 ]] && exit ${err};
 }
