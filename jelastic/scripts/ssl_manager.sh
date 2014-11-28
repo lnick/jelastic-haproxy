9ad7b24ba201e6b239a0d77abb233929a0b1bd45
SiryjVyiko <dz@hivext.net>
2014-11-28 18:00:11 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -3,6 +3,7 @@
 function _enableSSL(){
         doAction keystore DownloadKeys;
         cat /var/lib/jelastic/SSL/jelastic.crt /var/lib/jelastic/SSL/jelastic.key >> /var/lib/jelastic/SSL/jelastic.pem;
+        sed -i '/bind :80/a bind :443 ssl crt /var/lib/jelastic/SSL/jelastic.pem' ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf;
 }
 
 function _disableSSL(){
