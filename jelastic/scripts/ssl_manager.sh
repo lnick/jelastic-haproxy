2268464a1b0e4ad9e4d527ff2f1bc5cbf056d38d
SiryjVyiko <dz@hivext.net>
2014-11-28 18:08:20 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -4,10 +4,12 @@ function _enableSSL(){
         doAction keystore DownloadKeys;
         cat /var/lib/jelastic/SSL/jelastic.crt /var/lib/jelastic/SSL/jelastic.key >> /var/lib/jelastic/SSL/jelastic.pem;
         sed -i '/bind :80/a bind :443 ssl crt /var/lib/jelastic/SSL/jelastic.pem' ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf;
+        su - jelastic -c "${CARTRIDGE_HOME}/versions/1.5.8/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
 
 function _disableSSL(){
         doAction keystore remove;
-        rm -f /var/lib/jelastic/SSL/jelastic.pem;
         sed -i '/ssl crt/d' ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf;
+        rm -f /var/lib/jelastic/SSL/jelastic.pem;
+        su - jelastic -c "${CARTRIDGE_HOME}/versions/1.5.8/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
