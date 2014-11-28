b8461b3b7baf3b9bdae8d1904b632e03ccb5b206
SiryjVyiko <dz@hivext.net>
2014-11-28 17:22:07 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -2,8 +2,10 @@
 
 function _enableSSL(){
         doAction keystore DownloadKeys;
+        cat /var/lib/jelastic/SSL/jelastic.crt /var/lib/jelastic/SSL/jelastic.key >> /var/lib/jelastic/SSL/jelastic.pem;
 }
 
 function _disableSSL(){
         doAction keystore remove;
+        rm -f /var/lib/jelastic/SSL/jelastic.pem;
 }
