4d31ce9b678138335584e78361132aeb0f2e202f
SiryjVyiko <dz@hivext.net>
2015-01-19 09:40:49 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -11,5 +11,6 @@ function _disableSSL(){
         doAction keystore remove;
         sed -i '/ssl crt/d' ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf;
         rm -f /var/lib/jelastic/SSL/jelastic.pem;
+        sed -i '/'bind :443 ssl crt'/d' ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf;
         su - jelastic -c "${CARTRIDGE_HOME}/versions/1.5.8/bin/haproxy -D -f ${CARTRIDGE_HOME}/versions/1.5.8/conf/haproxy.conf -p ${CARTRIDGE_HOME}/run/haproxy.pid -sf $(cat ${CARTRIDGE_HOME}/run/haproxy.pid)"
 }
